0f09b95f0e0cefd2af724505712219cf6be2d845
happydilraj <happydilraj1022@gmail.com>
2023-07-22 16:13:02 +0530

    chatbot

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import Reducers from './reducers'
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+
+Kommunicate.init("2946d11aa8a208820361195ecfef3e096", {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true
+});
 
 const store = createStore(Reducers, compose(applyMiddleware(thunk)))
 
